d0b0b7f0804b852cfce17cb229a9d7038a1d09c0
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-23 21:01:57 +0900

    jest設定の追記

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  // 以下はなくてもよい
   // presets: 'ts-jest',
   // testEnvironment: 'node',
-  // testがあるファイルを指定
+  // testがあるファイルを指定(指定がないとnot found
   roots: ['<rootDir>/tests'],
+  // TS環境で必要
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
