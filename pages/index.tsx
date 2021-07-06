e54777660223aab30fecff5ddbc4872b3090e102
S-tanishi <tanichi.u.n.20@gmail.com>
2021-07-06 17:43:52 +0900

    scss環境の追加

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Button} from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <div>
+            <Button variant={"primary"}>ああ</Button>
+        </div>
+    )
+}
+
+export default Home
