5ff9d958e079b7d5e830e9515d95c4bf7540d454
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-12 21:07:53 +0900

    add:sample card layout の作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button} from "react-bootstrap";
+import {Button, Card} from "react-bootstrap";
 import Layout from "../src/components/_layouts/Layout";
 import {useRequireLogin} from "../src/hooks/useRequireLogin";
 
@@ -10,9 +10,13 @@ const Home = () => {
     return (
         <>
           <Layout>
-            <div>
-              <Button variant={"primary"}>ああ</Button>
-            </div>
+            <Card>
+              <Card.Header>Featured</Card.Header>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card>
           </Layout>
         </>
     )
