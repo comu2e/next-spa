f965d19e17014140ed5b64c237e635a873379159
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-12 10:15:41 +0900

    fix: header のlayout の組み方を修正

--- src/components/_layouts/Header.tsx
+++ src/components/_layouts/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Nav, NavDropdown, Navbar} from "react-bootstrap";
+import { Row, Col, Nav, NavDropdown, Navbar, Container} from "react-bootstrap";
 import Link from "next/link";
 import {useLoginUser} from "../../hooks/useLoginUser";
 
@@ -22,45 +22,34 @@ const Header = () => {
     return (
         <>
             <header className="py-1">
-                <Row>
-                    <Col md={"auto"} className="mr-auto d-flex align-items-center">
-                        <p className="site-title h5 m-0 pl-3">Sample HR</p>
-                    </Col>
-                    <Col md={"auto"}>
-                        <nav>
-                            <Nav className="me-auto">
-                                <Nav.Item>
-                                    <div className="nav-item nav-link">
-                                        <Link href="/">トップ</Link>
-                                    </div>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <div className="nav-item nav-link">
-                                        <Link href="/">労務管理</Link>
-                                    </div>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <div className="nav-item nav-link">
-                                        <Link href="/">ユーザー管理</Link>
-                                    </div>
-                                </Nav.Item>
+                <Navbar variant="dark">
+                    <Navbar.Brand href="#home">
+                        <p className="site-title h5 m-0 pl-3">Sample HR</p> </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">More deets</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Dank memes
+                        </Nav.Link>
+                        {loginUser ?
+                            <NavDropdown title={loginUser.name} id="nav-dropdown">
+                                <NavDropdown.Item><span onClick={handleLogout}>Logout</span></NavDropdown.Item>
+                            </NavDropdown>
+                            :
+                            <Nav.Item>
+                                <div className="nav-item nav-link">
+                                    <Link href="/login">Login</Link>
+                                </div>
+                            </Nav.Item>
+                        }
+                    </Nav>
+                </Navbar>
 
 
-                                    {loginUser ?
-                                        <NavDropdown title={loginUser.name} id="nav-dropdown">
-                                            <NavDropdown.Item><span onClick={handleLogout}>Logout</span></NavDropdown.Item>
-                                        </NavDropdown>
-                                        :
-                                        <Nav.Item>
-                                            <div className="nav-item nav-link">
-                                                <Link href="/login">Login</Link>
-                                            </div>
-                                        </Nav.Item>
-                                    }
-                            </Nav>
-                        </nav>
-                    </Col>
-                </Row>
             </header>
         </>
     )
