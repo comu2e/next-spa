cc15660c588fd81dabb437dfcb323efb1c072b2b
S-tanishi <tanichi.u.n.20@gmail.com>
2021-10-12 22:06:02 +0900

    add: 各種手続きのUI 骨格部分Sample の作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Card} from "react-bootstrap";
+import {Button, Card, Col, Container, Nav, Row} from "react-bootstrap";
 import Layout from "../src/components/_layouts/Layout";
 import {useRequireLogin} from "../src/hooks/useRequireLogin";
 
@@ -10,13 +10,88 @@ const Home = () => {
     return (
         <>
           <Layout>
-            <Card>
-              <Card.Header>Featured</Card.Header>
-              <Card.Text>
-                With supporting text below as a natural lead-in to additional content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card>
+              <Card>
+                <Card.Header>
+                  <Nav fill variant="tabs" defaultActiveKey="#first">
+                    <Nav.Item>
+                      <Nav.Link href="#first">労務 Active</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="#link">勤怠 Link</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="#link">経費申請 Link</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="#disabled" disabled>
+                        Disabled
+                      </Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Card.Header>
+                <Card.Body>
+
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>
+
+                    With supporting text below as a natural lead-in to additional content.
+                  </Card.Text>
+
+                  <Row className="mb-5">
+                    <Col md={6}>
+                      <Card bg="light">
+                        入社手続き
+                      </Card>
+                    </Col>
+                    <Col md={6}>
+                      <Card bg="light">
+                        退職手続き
+                      </Card>
+                    </Col>
+                  </Row>
+
+                  <Row className="mb-5">
+                    <Col md={6}>
+                      <Card bg="light" className="p-0 h-100">
+                        扶養追加
+                      </Card>
+                    </Col>
+                    <Col md={6}>
+                      <Card bg="light">
+                        住所変更
+                      </Card>
+                    </Col>
+                  </Row>
+
+                  <Row className="mb-5">
+                    <Col md={6}>
+                      <Card bg="light">
+                        氏名変更
+                      </Card>
+                    </Col>
+                    <Col md={6}>
+                      <Card bg="light">
+                        年末調整
+                      </Card>
+                    </Col>
+                  </Row>
+
+                  <Row className="mb-5">
+                    <Col md={6}>
+                      <Card bg="light">
+                        Web 給料明細
+                      </Card>
+                    </Col>
+                    <Col md={6}>
+                      <Card bg="light">
+                        あああああああ
+                      </Card>
+                    </Col>
+                  </Row>
+
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
           </Layout>
         </>
     )
